2812acbbcd178bf0c035b9db67f183a12a7d92d5
EC2 Default User <130027255+satoikokoro@users.noreply.github.com>
2023-05-10 06:45:11 +0000

    20230510

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  has_many :relationships, foreign_key: :followed_id
+  has_many :relationships, foreign_key: :followed_id, dependent: :destroy
   has_many :followings, through: :relationships, source: :follower
 
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :followed
 
   has_one_attached :profile_image
@@ -30,4 +30,20 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @users = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "backward-match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "#{word}")
+    else
+      @user = User.all
+    end
+  end
+
+
 end
